1132d3c2733b6136bb3773c3e8a7d98357be20f4
Leithen <leithen113@gmail.com>
2021-10-31 09:31:10 -0600

    Setting up inquirer from NPM

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [user, github] = profileDataArgs
+const inquirer = require('inquirer');
 
-fs.writeFile('index.html', generatePage(user, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(user, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
