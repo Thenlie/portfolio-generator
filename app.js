b46be6b990b719c77c264402f8773926d4bbed9b
Leithen <leithen113@gmail.com>
2021-10-31 10:22:32 -0600

    Added confirm for about section

--- app.js
+++ app.js
@@ -35,9 +35,21 @@ const promptUser = () => {
             }
         }
     }, {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About Me" section?',
+        default: true
+    }, {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself'
+        message: 'Provide some information about yourself',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }]);
 };
 
